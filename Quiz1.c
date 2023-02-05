3f27f25682e8b1da3116fa45a7ea839818e2a9b1
Reza Choudhury <116828953+pootwizard@users.noreply.github.com>
2023-02-05 16:09:58 -0600

    Add files via upload

---
+++ Quiz1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void){
+		//printf("string format", argument);
+	int n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0, n6 = 0, n7 = 0, n8 = 0, n9 = 0, n10 = 0;
+	cout << "Please enter the 10 digits phone number : ";
+	cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6 >> n7 >> n8 >> n9 >> n10;
+	return 0;
+	}
+
